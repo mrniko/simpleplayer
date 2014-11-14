1f81adc6e756434b510b3df7f9c8c444f317646a
Sergii Zhuk <sergey.public@gmail.com>
2014-11-14 21:52:57 +0200

    Trial Robolectric 2.3 test

---
+++ src/test/java/org.sergez.splayer/activity/PreferencesActivityTest.java
@@ -0,0 +1,33 @@
+package org.sergez.splayer.activity;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * @author Sergii Zhuk
+ *         Date: 29.09.2014
+ *         Time: 11:37
+ */
+@Config(emulateSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class PreferencesActivityTest {
+
+    private PreferencesActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.buildActivity(PreferencesActivity.class).create().get();
+    }
+
+    @Test
+    public void checkActivityNotNull() throws Exception {
+        Assert.assertNotNull(activity);
+
+    }
+
+}
