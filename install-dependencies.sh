15f6e3d93cf0c29b62e7901d51a787d93ca0b606
Sergii Zhuk <sergey.public@gmail.com>
2014-11-16 16:46:41 +0200

    Circle CI configuration added

---
+++ install-dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Fix the CircleCI path
+export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+
+DEPS="$ANDROID_HOME/installed-dependencies"
+
+if [ ! -e $DEPS ]; then
+  echo y | android update sdk -u -a -t android-19 &&
+  echo y | android update sdk -u -a -t platform-tools &&
+  echo y | android update sdk -u -a -t extra &&
+  echo y | android update sdk -u -a -t build-tools-19.1.0 &&
+  echo y | android update sdk -u -a -t addon-google_apis-google-19 &&
+  touch $DEPS
+fi
