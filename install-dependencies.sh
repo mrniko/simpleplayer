003196d07fd2fc001c56d4281394882f1e5a7786
Sergii Zhuk <sergey.public@gmail.com>
2015-11-16 00:02:19 +0100

    Travis build fix

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,10 +6,10 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
-  echo y | android update sdk -u -a -t android-19 &&
+  echo y | android update sdk -u -a -t android-23 &&
   echo y | android update sdk -u -a -t platform-tools &&
   echo y | android update sdk -u -a -t extra &&
-  echo y | android update sdk -u -a -t build-tools-21.1.1 &&
-  echo y | android update sdk -u -a -t addon-google_apis-google-19 &&
+  echo y | android update sdk -u -a -t build-tools-23.0.1 &&
+  echo y | android update sdk -u -a -t addon-google_apis-google-23 &&
   touch $DEPS
 fi
